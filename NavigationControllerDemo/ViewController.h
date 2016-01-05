43afa0c9dd0b94adab3c26f2c0acaf08bb64c12f
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2016-01-05 10:22:13 -0200

    Initial Commit

---
+++ NavigationControllerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NavigationControllerDemo
+//
+//  Created by Rafael Ferreira on 1/5/16.
+//  Copyright © 2016 Data Empire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NavigationControllerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NavigationControllerDemo
+//
+//  Created by Rafael Ferreira on 1/5/16.
+//  Copyright © 2016 Data Empire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
